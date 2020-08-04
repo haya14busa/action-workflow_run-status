a3acdbade80e0d7e03d5bc37d445bfb134854010
haya14busa <haya14busa@gmail.com>
2020-08-04 06:11:32 +0000

    dump context

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import * as stateHelper from './state-helper'
 
 async function run(): Promise<void> {
   try {
+    // const token = core.getInput('github_token');
+    // const octokit = github.getOctokit(token);
+    const context = github.context
+    core.warning(JSON.stringify(context, null, 2))
+    // octokit.status
+    // octokit.repos.createCommitStatus({
+    //   owner,
+    //   repo,
+    //   sha,
+    //   state,
+    // });
     core.warning('main')
   } catch (error) {
     core.setFailed(error.message)
